d068326100e47d46b6645f984495c1f9757ccb7b
Ângelo Fracisco Souza de Araújo <angelo.francisco@trackcash.com.br>
2021-08-17 16:13:37 -0300

    first commit

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SeriesController{
+
+    public function index()
+    {
+        return [
+            "Gray's Anatomy",
+            "Lost",
+        ];
+    }
+}
